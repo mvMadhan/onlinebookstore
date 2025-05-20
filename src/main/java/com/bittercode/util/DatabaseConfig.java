da2f51de8b203d012d55edf1ad8e4a206910235f
mvMadhan <126807110+mvMadhan@users.noreply.github.com>
2025-05-20 19:57:32 +0530

    Update DatabaseConfig.java

--- src/main/java/com/bittercode/util/DatabaseConfig.java
+++ src/main/java/com/bittercode/util/DatabaseConfig.java
@@ -7,24 +7,34 @@ import java.util.Properties;
 class DatabaseConfig {
 
     static Properties prop = new Properties();
-    static {
 
+    static {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         InputStream input = classLoader.getResourceAsStream("application.properties");
 
         try {
-            prop.load(input);
+            if(input != null) {
+                prop.load(input);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public final static String DRIVER_NAME = prop.getProperty("db.driver");
-    public final static String DB_HOST = prop.getProperty("db.host");
-    public final static String DB_PORT = prop.getProperty("db.port");
-    public final static String DB_NAME = prop.getProperty("db.name");
-    public final static String DB_USER_NAME = prop.getProperty("db.username");
-    public final static String DB_PASSWORD = prop.getProperty("db.password");
-    public final static String CONNECTION_STRING = DB_HOST + ":" + DB_PORT + "/" + DB_NAME;
+    private static String getEnvOrProp(String envKey, String propKey) {
+        String val = System.getenv(envKey);
+        if (val != null && !val.isEmpty()) {
+            return val;
+        }
+        return prop.getProperty(propKey);
+    }
 
+    public final static String DRIVER_NAME = getEnvOrProp("DB_DRIVER", "db.driver");
+    public final static String DB_HOST = getEnvOrProp("DB_HOST", "db.host");
+    public final static String DB_PORT = getEnvOrProp("DB_PORT", "db.port");
+    public final static String DB_NAME = getEnvOrProp("DB_NAME", "db.name");
+    public final static String DB_USER_NAME = getEnvOrProp("DB_USER", "db.username");
+    public final static String DB_PASSWORD = getEnvOrProp("DB_PASS", "db.password");
+
+    public final static String CONNECTION_STRING = "jdbc:mysql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME + "?useSSL=false&allowPublicKeyRetrieval=true";
 }
